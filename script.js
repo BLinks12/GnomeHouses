eed84927f6a37397d423ec2af59c4cd4a2121f49
BLinks12 <honorforgeai@gmail.com>
2024-11-30 23:11:30 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Array of image paths
+let images = [];
+for (let i = 1; i <= 14; i++) {
+  images.push(`images/gnome_house${i}.png`);
+}
+
+// Event listener for the button
+document.getElementById('randomize-button').addEventListener('click', displayRandomImage);
+
+// Function to display a random image
+function displayRandomImage() {
+  let randomIndex = Math.floor(Math.random() * images.length);
+  document.getElementById('canvas-image').src = images[randomIndex];
+}
+
+// Initial random image on page load
+window.onload = function() {
+  displayRandomImage();
+};
